ea0a70997759f3dcb54b6cc6d3ad8c47dcb8da70
Yearn <2992454035@qq.com>
2025-03-24 16:04:16 +0800

    config

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export interface ProfileConfig {
 //favicon.ico 图标直接替换/src/app文件夹中的图片即可 以favicon.ico命名
 export const profileConfig: ProfileConfig = {
     name: "Yearn",
-    bio: "Acmer",
+    bio: "Bystander",
     imageSrc: "/avatar2.jpg",
     //如果放在public文件夹下，则直接/即可
     //如果图片是链接请在next.config.js中配置images:{domains: ["masttf.fun"],}
